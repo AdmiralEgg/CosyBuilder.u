b9d24032c83a7ddb6ba00efa450f9062bd056237
AdmiralEgg <chris.alexander87@gmail.com>
2024-04-01 20:24:22 +0100

    Update CursorData.cs

--- Assets/_Project/Scripts/Manager/CursorData.cs
+++ Assets/_Project/Scripts/Manager/CursorData.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using static DictionarySerialization;
-using static UnityEditor.PlayerSettings;
 
 public class CursorData : MonoBehaviour
 {
